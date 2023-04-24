09fa6fc10a9333fc707d34f23e564c59c118ac2b
in1dw3 <in1dw3@HZ352017.elorrieta.int>
2023-04-24 20:56:45 +0200

    eliminada rama master

---
+++ aam/src/es/elorrieta/aam/model/bbdd/pojo/junitSprint1/BrandTest.java
@@ -0,0 +1,21 @@
+package es.elorrieta.aam.model.bbdd.pojo.junitSprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import es.elorrieta.aam.model.bbdd.pojo.Brand;
+
+class BrandTest {
+	private Brand brand = null;
+
+	public BrandTest() {
+		brand = new Brand();
+	}
+
+	@Test
+	void contructorTest() {
+		asssertEquals();
+	}
+
+}
