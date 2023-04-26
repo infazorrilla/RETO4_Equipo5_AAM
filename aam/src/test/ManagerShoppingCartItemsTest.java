05d47ed7b4c4ffad080b0d8715cb379efcfceec4
Admin <Admin@172.16.6.57>
2023-04-26 16:34:32 +0200

    JUNIT shoppingcartItemManager

---
+++ aam/src/test/ManagerShoppingCartItemsTest.java
@@ -0,0 +1,129 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import es.elorrieta.aam.model.bbdd.manager.ManagerShoppingCartItems;
+import es.elorrieta.aam.model.bbdd.pojo.ProductItem;
+import es.elorrieta.aam.model.bbdd.pojo.ShoppingCart;
+import es.elorrieta.aam.model.bbdd.pojo.ShoppingCartItem;
+
+class ManagerShoppingCartItemsTest {
+
+	private ShoppingCartItem shoppingCartItem = null;
+	private ManagerShoppingCartItems managerShoppingCartItems = null;
+
+	public ManagerShoppingCartItemsTest() {
+		shoppingCartItem = new ShoppingCartItem();
+		managerShoppingCartItems = new ManagerShoppingCartItems();
+	}
+
+	@Test
+	void insertSelectUpdateDeleteTest() {
+
+		// insertTest()
+		// sabiendo que id_productItem = 1 y id_shoppingcart = 21
+
+		ProductItem ProductItem = new ProductItem();
+		ProductItem.setId(1);
+		shoppingCartItem.setProductItem(ProductItem);
+		ShoppingCart shoppingCart = new ShoppingCart();
+		shoppingCart.setId(21);
+		shoppingCartItem.setShoppingCart(shoppingCart);
+		shoppingCartItem.setPrice(132);
+		shoppingCartItem.setQuantity(12);
+
+		// insertamos shoppingCartItem en la tabla shoppingCartItems en la base de datos
+
+		try {
+			managerShoppingCartItems.insert(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+
+		// hacemos el select buscandolo por id_shoppingcart y id_product_item
+
+		List<ShoppingCartItem> shoppingCartItems = new ArrayList<ShoppingCartItem>();
+		try {
+			shoppingCartItems = managerShoppingCartItems.select(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+		// ahora comprobamos que se ha insertado correctamente
+
+		assertEquals(shoppingCartItems.get(0).getShoppingCart().getId(), shoppingCartItem.getShoppingCart().getId());
+		assertEquals(shoppingCartItems.get(0).getProductItem().getId(), shoppingCartItem.getProductItem().getId());
+		assertEquals(shoppingCartItems.get(0).getPrice(), 132, shoppingCartItem.getPrice());
+		assertEquals(shoppingCartItems.get(0).getQuantity(), shoppingCartItem.getQuantity());
+
+		// updateTest()
+
+		shoppingCartItem.setId(shoppingCartItems.get(0).getId());
+
+		// update
+
+		shoppingCartItem.setPrice(250);
+
+		shoppingCartItem.setQuantity(5);
+
+		try {
+			managerShoppingCartItems.update(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+
+		// hacemos el select
+		try {
+			shoppingCartItems = managerShoppingCartItems.select(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+
+		// comprobamos si los 2 objetos son iguales
+
+		assertEquals(shoppingCartItems.get(0).toString(), shoppingCartItem.toString());
+		assertEquals(shoppingCartItems.get(0).getPrice(), 250, shoppingCartItem.getPrice());
+		assertEquals(shoppingCartItems.get(0).getQuantity(), shoppingCartItem.getQuantity());
+
+		// DeleteTest()
+		// se espera shoppingCartItems --> null.
+
+		// borramos shoppingCartItem de la base de datos
+
+		try {
+			managerShoppingCartItems.delete(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+
+		// hacemos el select
+
+		try {
+			shoppingCartItems = managerShoppingCartItems.select(shoppingCartItem);
+		} catch (SQLException e) {
+			System.out.println("SQLException:  " + e);
+		} catch (Exception e) {
+			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
+		}
+
+		// comprobamos que shoppingCartItems = null
+		assertNull(shoppingCartItems);
+	}
+
+}
--- aam/src/test/ManagerShoppingCartTest.java
+++ aam/src/test/ManagerShoppingCartTest.java
@@ -24,12 +24,14 @@ class ManagerShoppingCartTest {
 	}
 
 	@Test
-	void insertSelectTest() {
+	void insertSelectUpdateDelteTest() {
+		// insertTest()
+
 		shoppingCart.setTotalPrice(125.68);
 		shoppingCart.setDescount(0.3);
 		Date createdAt = new Date();
 		shoppingCart.setCreatedAt(createdAt);
-
+// insertamos shoppingcart en la base de datos.
 		try {
 			managerShoppingCart.insert(shoppingCart);
 		} catch (SQLException e) {
@@ -37,7 +39,7 @@ class ManagerShoppingCartTest {
 		} catch (Exception e) {
 			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
 		}
-
+//hacer el select buscandolo por fecha de creacion
 		List<ShoppingCart> shoppingcart = new ArrayList<ShoppingCart>();
 
 		try {
@@ -48,6 +50,8 @@ class ManagerShoppingCartTest {
 			System.out.println(" " + e);
 		}
 
+		// comprobamos comparando los atributos de los objetos son iguales
+
 		assertEquals(shoppingcart.size(), 1);
 
 		assertEquals(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(shoppingcart.get(0).getCreatedAt()),
@@ -55,10 +59,14 @@ class ManagerShoppingCartTest {
 		assertEquals(shoppingcart.get(0).getTotalPrice(), 125.68, shoppingCart.getTotalPrice());
 		assertEquals(shoppingcart.get(0).getDescount(), 0.3, shoppingCart.getDescount());
 
+		// updateTest()
+
 		shoppingCart.setId(shoppingcart.get(0).getId());
 
 		shoppingCart.setTotalPrice(50);
 		shoppingCart.setDescount(0.2);
+
+		// update
 
 		try {
 			managerShoppingCart.update(shoppingCart);
@@ -67,6 +75,7 @@ class ManagerShoppingCartTest {
 		} catch (Exception e) {
 			System.out.println("NO se ha podido conectar ala base de datos:  " + e);
 		}
+		// hacer el select
 
 		try {
 			shoppingcart = managerShoppingCart.select(shoppingCart);
@@ -75,13 +84,15 @@ class ManagerShoppingCartTest {
 		} catch (Exception e) {
 			System.out.println("  " + e);
 		}
-
+//comprobamos que los datos se han modificado correctamente 
 		assertEquals(shoppingcart.get(0).toString(), shoppingCart.toString());
 		assertEquals(shoppingcart.get(0).getTotalPrice(), 50, shoppingCart.getTotalPrice());
 		assertEquals(shoppingcart.get(0).getDescount(), 0.1, shoppingCart.getDescount());
 		assertEquals(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(shoppingcart.get(0).getCreatedAt()),
 				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(shoppingCart.getCreatedAt()));
-
+//deleteTest()
+		// borramos shoppingcart por id
+		// se espera shoppingcart --> null
 		try {
 			managerShoppingCart.delete(shoppingCart);
 		} catch (SQLException e) {
@@ -97,7 +108,7 @@ class ManagerShoppingCartTest {
 		} catch (Exception e) {
 			System.out.println(" " + e);
 		}
-
+// comprobamos que shoppingcart = null
 		assertNull(shoppingcart);
 
 	}
