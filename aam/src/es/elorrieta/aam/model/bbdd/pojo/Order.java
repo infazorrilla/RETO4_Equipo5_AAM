9616b874cd5609445b205b1e6896dc0e43fd5ec3
vaain <vaain@DESKTOP-E7E5TPS>
2023-04-26 16:57:52 +0200

    Update JUnit of Order & Payment

--- aam/src/es/elorrieta/aam/model/bbdd/pojo/Order.java
+++ aam/src/es/elorrieta/aam/model/bbdd/pojo/Order.java
@@ -4,130 +4,126 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-import es.elorrieta.aam.model.bbdd.pojo.manager.ManagerOrders;
-
 public class Order  implements Serializable {
 
 	private static final long serialVersionUID = 7011816090907660930L;
-
+	
 	// PK
 	private int id = 0;
-
+	
 	// FK: the relationship between Order and payment is 1 to 1.
 	private Payment payment = null;
-
+	
 	// FK: the relationship between Order and ShoppingCart is 1 to 1.
 	private ShoppingCart shoppingCart = null;
-
+	
 	// FK: the relationship between Order and Customer is 1 to 1.
 	private Customer customer = null;
-
+	
 	// FK: the relationship between Order and Address is 1 to 1.
 	private Address address = null;
-
+	
 	// FK: the relationship between Order and EmployeeManagedOrders is 1 to 1.
 	private EmployeeManagedOrders EmployeeManagedOrders = null;
-
+	
 	// ATTRIBUTES
 	private Date deliveryDate = null;
 	private Date orderDate = null;
 	private double totalPrice = 0;
 	private Status status = null;
-
+	
 	public enum Status {
 		Pending, AwaitingShipment, Shipped, Cancelled
 
 	}
-
-
-	// Getters and Setters
-
+	
+	// GETTERS AND SETTERES
 	public int getId() {
 		return id;
 	}
-
+	
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	
 	public Date getDeliveryDate() {
 		return deliveryDate;
 	}
-
+	
 	public void setDeliveryDate(Date deliveryDate) {
 		this.deliveryDate = deliveryDate;
 	}
-
+	
 	public Date getOrderDate() {
 		return orderDate;
 	}
-
+	
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
-
+	
 	public double getTotalPrice() {
 		return totalPrice;
 	}
-
+	
 	public void setTotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
-
+	
 	public Status getStatus() {
 		return status;
 	}
-
+	
 	public void setStatus(Status status) {
 		this.status = status;
 	}
-
+	
 	public Payment getPayment() {
 		return payment;
 	}
-
+	
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-
+	
 	public ShoppingCart getShoppingCart() {
 		return shoppingCart;
 	}
-
+	
 	public void setShoppingCart(ShoppingCart shoppingCart) {
 		this.shoppingCart = shoppingCart;
 	}
-
+	
 	public Customer getCustomer() {
 		return customer;
 	}
-
+	
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-
+	
 	public Address getAddress() {
 		return address;
 	}
-
+	
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-
+	
 	public EmployeeManagedOrders getEmployeeManagedOrders() {
 		return EmployeeManagedOrders;
 	}
-
+	
 	public void setEmployeeManagedOrders(EmployeeManagedOrders employeeManagedOrders) {
 		EmployeeManagedOrders = employeeManagedOrders;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(EmployeeManagedOrders, address, customer, deliveryDate, id, orderDate, payment,
 				shoppingCart, status, totalPrice);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -144,7 +140,7 @@ public class Order  implements Serializable {
 				&& Objects.equals(shoppingCart, other.shoppingCart) && status == other.status
 				&& Double.doubleToLongBits(totalPrice) == Double.doubleToLongBits(other.totalPrice);
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Order [id=" + id + ", deliveryDate=" + deliveryDate + ", orderDate=" + orderDate + ", totalPrice="
--- aam/src/es/elorrieta/aam/model/bbdd/pojo/Payment.java
+++ aam/src/es/elorrieta/aam/model/bbdd/pojo/Payment.java
@@ -4,71 +4,67 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-import es.elorrieta.aam.model.bbdd.pojo.manager.ManagerPayments;
-
 public class Payment  implements Serializable {
 
 	private static final long serialVersionUID = -6152476064974115544L;
-
+	
 	// PK
 	private int id = 0;
-
+	
 	// FK: the relationship between payment and order is 1 to 1.
 	private Order order = null;
-
+	
 	// ATTRIBUTES
 	private String iban = null;
 	private String cvv = null;
 	private Date expirationDate = null;
-
 	
-
 	// GETTERS AND SETTERES
 	public int getId() {
 		return id;
 	}
-
+	
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	
 	public String getIban() {
 		return iban;
 	}
-
+	
 	public void setIban(String iban) {
 		this.iban = iban;
 	}
-
+	
 	public String getCvv() {
 		return cvv;
 	}
-
+	
 	public void setCvv(String cvv) {
 		this.cvv = cvv;
 	}
-
+	
 	public Date getExpirationDate() {
 		return expirationDate;
 	}
-
+	
 	public void setExpirationDate(Date expirationDate) {
 		this.expirationDate = expirationDate;
 	}
-
+	
 	public Order getOrder() {
 		return order;
 	}
-
+	
 	public void setOrder(Order order) {
 		this.order = order;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(cvv, expirationDate, iban, id, order);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -81,7 +77,7 @@ public class Payment  implements Serializable {
 		return Objects.equals(cvv, other.cvv) && Objects.equals(expirationDate, other.expirationDate)
 				&& Objects.equals(iban, other.iban) && id == other.id && Objects.equals(order, other.order);
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Payment [id=" + id + ", iban=" + iban + ", cvv=" + cvv + ", expirationDate=" + expirationDate
